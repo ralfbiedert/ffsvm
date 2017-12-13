a5522201e24f0d2e4d43023bd1b074a0a4242d37
Ralf Biedert <rb@xr.io>
2017-12-13 22:19:07 +0100

    Applying even more refactoring …

--- src/triangularmatrix.rs
+++ src/triangularmatrix.rs
@@ -8,15 +8,18 @@ use util;
 /// Square triangular matrix.
 /// 
 /// A data structure for matrices that look like this:
-///
-/// ```         
-///      j --->
-///         0   1   2   3
-///    i 0      x   x   x
-///    | 1          x   x
-///    v 2              x
-///      3
-/// ```
+///     /// THIS SHIT BREAKS COMPILATION ...
+
+/*
+/
+/      j ---
+/         0   1   2   3
+/    i 0      x   x   x
+/    | 1          x   x
+/      2              x
+/      3
+
+*/
 /// 
 /// The values will be stored from "left to right", from 
 /// "up" to "down".
@@ -48,19 +51,20 @@ where
 
     /// 
     /// Setting
-    /// 
-    /// ```         
-    ///      j --->
-    ///         0   1   2   3
-    ///    i 0      x   x   x
-    ///    | 1          x   x
-    ///    v 2              x
-    ///      3
-    /// ```
-    /// 0,0 -> invalid
-    /// 0,1 -> 0
-    /// 0,3 -> 3
-    /// 2,3 -> 3
+    /// THIS SHIT BREAKS COMPILATION ...
+//    / 
+//    /          
+//    /      j ---
+//    /         0   1   2   3
+//    /    i 0      x   x   x
+//    /    | 1          x   x
+//    /      2              x
+//    /      3
+//    / 
+//    / 0,0 -> invalid
+//    / 0,1 -> 0
+//    / 0,3 -> 3
+//    / 2,3 -> 3
     pub fn set(&self, i: usize, j: usize) {
         debug_assert!(i < j);
         debug_assert!(i < self.dimension);
