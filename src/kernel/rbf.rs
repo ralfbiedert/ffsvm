b15b5233b605d377b69d15457c5466daab4f53bd
Ralf Biedert <rb@xr.io>
2018-08-05 12:31:42 +0200

    Now with dynamic scalar / SIMD selection based on vector length.

--- src/kernel/rbf.rs
+++ src/kernel/rbf.rs
@@ -5,7 +5,7 @@ use crate::parser::ModelFile;
 use crate::random::Random;
 use crate::vectors::SimdOptimized;
 
-use packed_simd::f32x8;
+use packed_simd::{f32x16, f32x4, f32x8};
 use rand::random;
 
 #[cfg(feature = "serde")]
@@ -16,6 +16,32 @@ use serde::{Deserialize, Serialize};
 #[doc(hidden)]
 pub struct RbfKernel {
     pub gamma: f32,
+}
+
+macro_rules! compute_kernel_impl {
+    ($fn:ident, $vectype:ty) => {
+        #[inline]
+        fn $fn(sv: &[f32], feature: &[f32], gamma: f32) -> f64 {
+            #[allow(non_camel_case_types)]
+            type f32s = $vectype;
+
+            let width = f32s::lanes();
+            let steps = sv.len() / width;
+
+            let mut sum = f32s::splat(0.0);
+
+            for i in 0..steps {
+                let a = f32s::from_slice_unaligned(&sv[i * width..]);
+                let b = f32s::from_slice_unaligned(&feature[i * width..]);
+                sum += (a - b) * (a - b);
+            }
+
+            // This seems to be the single-biggest CPU spike: saving back kernel_values,
+            // and computing exp() (saving back seems to have 3x time impact over exp(),
+            // but I might misread "Instruments" for that particular one).
+            f64::from((-gamma * sum.sum()).exp())
+        }
+    };
 }
 
 #[inline]
@@ -29,54 +55,25 @@ fn compute_inner_kernel_scalar(sv: &[f32], feature: &[f32], gamma: f32) -> f64 {
     f64::from((-gamma * sum).exp())
 }
 
-#[inline]
-fn compute_inner_kernel_simdf32x8(sv: &[f32], feature: &[f32], gamma: f32) -> f64 {
-    #[allow(non_camel_case_types)]
-    type f32s = f32x8;
-
-    let width = f32s::lanes();
-    let steps = sv.len() / width;
-
-    let mut sum = f32s::splat(0.0);
-
-    for i in 0..steps {
-        // When benchmarking `csvm_predict_sv1024_attr1024_problems1` with AVX2:
-
-        // 238,928 ns / iter
-        let a = unsafe { f32s::from_slice_aligned_unchecked(&sv[i * width..]) };
-        let b = unsafe { f32s::from_slice_aligned_unchecked(&feature[i * width..]) };
-
-        // While I would love to have purely safe code, anything that's not `unchecked` will
-        // reduce the overall classification speed by 50%! The problem I see with `packed_simd` is
-        // that it treats every f32s independently, so it forces checks on every f32s load operation.
-        // It would be much nicer having an API that does a sanity check once, and then just iterates.
-
-        // 237,541 ns / iter
-        // let a = unsafe { f32s::from_slice_unaligned_unchecked(&sv[i * width..]) };
-        // let b = unsafe { f32s::from_slice_unaligned_unchecked(&feature[i * width..]) };
-
-        // 343,970 ns / iter
-        // let a = f32s::from_slice_aligned(&sv[i * width..]);
-        // let b = f32s::from_slice_aligned(&feature[i * width..]);
-
-        // 363,796 ns / iter
-        // let a = f32s::from_slice_unaligned(&sv[i * width..]);
-        // let b = f32s::from_slice_unaligned(&feature[i * width..]);
-        sum += (a - b) * (a - b);
-    }
-
-    // This seems to be the single-biggest CPU spike: saving back kernel_values,
-    // and computing exp() (saving back seems to have 3x time impact over exp(),
-    // but I might misread "Instruments" for that particular one).
-    f64::from((-gamma * sum.sum()).exp())
-}
+compute_kernel_impl!(compute_inner_kernel_simdf32x16, f32x16);
+compute_kernel_impl!(compute_inner_kernel_simdf32x8, f32x8);
+compute_kernel_impl!(compute_inner_kernel_simdf32x4, f32x4);
 
 impl Kernel for RbfKernel {
     fn compute(&self, vectors: &SimdOptimized<f32>, feature: &[f32], kernel_values: &mut [f64]) {
         // According to Instruments, for realistic SVMs and problems, the VAST majority of our
         // CPU time is spent in this loop.
         for (i, sv) in vectors.into_iter().enumerate() {
-            kernel_values[i] = compute_inner_kernel_simdf32x8(sv, feature, self.gamma);
+            assert_eq!(sv.len(), feature.len());
+
+            let len = sv.len();
+
+            kernel_values[i] = match len {
+                _ if len % 16 == 0 => compute_inner_kernel_simdf32x16(sv, feature, self.gamma),
+                _ if len % 8 == 0 => compute_inner_kernel_simdf32x8(sv, feature, self.gamma),
+                _ if len % 4 == 0 => compute_inner_kernel_simdf32x4(sv, feature, self.gamma),
+                _ => compute_inner_kernel_scalar(sv, feature, self.gamma),
+            };
         }
     }
 }
