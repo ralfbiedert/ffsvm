9dc3919985d774dcd0591267ffdcac9e2b9ebf7a
Ralf Biedert <rb@xr.io>
2018-08-11 15:43:34 +0200

    Producing test data for unit tests.

---
+++ tests/gen-data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import random
+
+CLASSES = 8
+SAMPLES_PER_CLASS = 4
+ATTRIBUTES = 8
+
+with open('problem.csv', "w") as csv:
+    with open('problem.in', "w") as f:
+        for c in range(CLASSES):
+
+            # We'll write `SAMPLES_PER_CLASS * CLASSES` lines.
+            for _ in range(SAMPLES_PER_CLASS):
+                base = (c / CLASSES) + 0.0001
+
+                f.write(str(c) + " ")
+
+                nums = []
+
+                for j in range(ATTRIBUTES):
+                    num = base + (base**3 * random.random())
+                    rest = str(j) + ":" + str(num) + " "
+                    nums.append(str(num))
+                    f.write(rest)
+
+                csv.write(",".join(nums) + "\n")
+                f.write("\n")
