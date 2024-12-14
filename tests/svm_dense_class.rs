ed04e2c58001a59c97d0d650919053312ad884bd
Ralf Biedert <rb@xr.io>
2024-12-14 16:54:52 +0100

    Enable tests that previously ICEed.

--- tests/svm_dense_class.rs
+++ tests/svm_dense_class.rs
@@ -44,10 +44,10 @@ mod svm_dense_class {
     test_model!(m_csvm_sigmoid_prob, "m_csvm_sigmoid_prob.libsvm", true, [0, 5], [0, 7]); // apparently `libSVM` gets this wrong
 
     // Temporarily disabled as they trigger ICE in Rust Nightly
-    //    test_model!(m_csvm_linear, "m_csvm_linear.libsvm", false, [0, 7], []);
-    //    test_model!(m_csvm_poly, "m_csvm_poly.libsvm", false, [0, 7], []);
-    //    test_model!(m_csvm_rbf, "m_csvm_rbf.libsvm", false, [0, 7], []);
-    //    test_model!(m_csvm_sigmoid, "m_csvm_sigmoid.libsvm", false, [0, 5], []);
+    test_model!(m_csvm_linear, "m_csvm_linear.libsvm", false, [0, 7], [0, 0]);
+    test_model!(m_csvm_poly, "m_csvm_poly.libsvm", false, [0, 7], [0, 0]);
+    test_model!(m_csvm_rbf, "m_csvm_rbf.libsvm", false, [0, 7], [0, 0]);
+    test_model!(m_csvm_sigmoid, "m_csvm_sigmoid.libsvm", false, [0, 5], [0, 0]);
 
     // NUSVM
     test_model!(m_nusvm_linear_prob, "m_nusvm_linear_prob.libsvm", true, [0, 7], [0, 7]);
@@ -56,8 +56,8 @@ mod svm_dense_class {
     test_model!(m_nusvm_sigmoid_prob, "m_nusvm_sigmoid_prob.libsvm", true, [0, 7], [0, 7]);
 
     // Temporarily disabled as they trigger ICE in Rust Nightly
-    //    test_model!(m_nusvm_linear, "m_nusvm_linear.libsvm", false, [0, 7], []);
-    //    test_model!(m_nusvm_poly, "m_nusvm_poly.libsvm", false, [0, 7], []);
-    //    test_model!(m_nusvm_rbf, "m_nusvm_rbf.libsvm", false, [0, 7], []);
-    //    test_model!(m_nusvm_sigmoid, "m_nusvm_sigmoid.libsvm", false, [0, 7], []);
+    test_model!(m_nusvm_linear, "m_nusvm_linear.libsvm", false, [0, 7], [0, 0]);
+    test_model!(m_nusvm_poly, "m_nusvm_poly.libsvm", false, [0, 7], [0, 0]);
+    test_model!(m_nusvm_rbf, "m_nusvm_rbf.libsvm", false, [0, 7], [0, 0]);
+    test_model!(m_nusvm_sigmoid, "m_nusvm_sigmoid.libsvm", false, [0, 7], [0, 0]);
 }
